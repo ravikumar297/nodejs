95b335ef567139e17ae450bd994424de2fb5354b
ravikumar297 <msmnr29@gmail.com>
2024-07-17 07:10:42 +0530

    user api check

--- models/user.model.js
+++ models/user.model.js
@@ -3,7 +3,7 @@ const db = require('../config/db.config');
 class User {
     
     constructor(user) {
-        this.first_name = user.first_name;
+        this.first_name = user.first_name; 
         this.last_name = user.last_name;
         this.mail = user.mail;
         this.password = user.password;
